21f10bae8c0e0417a5da5a176d1d0e0bd9268a02
Plamen Stilyianov <plamen.stilyianov@outlook.com>
2024-03-21 20:01:41 +0000

    Added permutation function and refactored it

--- utils/helper.py
+++ utils/helper.py
@@ -1,33 +1,114 @@
 import warnings
 import numpy as np
 import pandas as pd
+from finta import TA as finta
 import matplotlib.pyplot as plt
 from plotly.figure_factory import create_table
-from finta import TA
 from sklearn.pipeline import Pipeline
+from sklearn.inspection import permutation_importance
 from sklearn.compose import make_column_transformer, make_column_selector
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
 
 warnings.filterwarnings("ignore")
 
+COLUMNS = ['open', 'high', 'low', 'close', 'volume']
 
-def add_indicators(df_stock):
+
+def add_pct_changes(df_stock):
+    columns = ['open', 'high', 'low', 'close', 'volume', 'trade_count', 'vwap']
+    df = df_stock[columns]
+    df['HL_PCT'] = (df['high'] - df['low']) / df['close'] * 100.0
+    df['PCT_change'] = (df['close'] - df['open']) / df['open'] * 100.0
+    return df
+
+
+# def add_indicators_pd_ta(df_stock):
+#     indicators = ['KAMA', 'RSI', 'DEMA', 'PPO', 'SMA', 'WMA', 'MOM', 'ROC', 'TRIX', 'TRIMA', 'EMA', 'TEMA', 'MACD']
+#     df_ta = df_stock['vwap']
+#     for indicator in indicators:
+#         df = None
+#         try:
+#             df = eval('ta.' + str(indicator).lower() + '(df_ta)')
+#         except:
+#             print('Invalid Indicator: ' + indicator)
+#
+#         if isinstance(df, pd.Series):
+#             df = df.to_frame()
+#         # Appropriate labels on each column
+#         df = df.add_prefix(indicator + '_')
+#         # Join merge dataframes based on the date
+#         df_stock = df_stock.merge(df, left_index=True, right_index=True)
+#
+#     df = None
+#     df = eval('ta.' + str('bbands') + '(df_ta)')
+#     df_stock = df_stock.merge(df, left_index=True, right_index=True)
+#     # Fix labels
+#     df_stock.columns = df_stock.columns.str.replace(' ', '_')
+#     df_stock.index.name = 'date'
+#     return df_stock
+#
+#
+# def add_indicators_talib(df_stock):
+#     indicators = ['KAMA', 'RSI', 'DEMA', 'PPO', 'SMA', 'WMA', 'MOM', 'ROC', 'TRIX', 'TRIMA', 'EMA', 'TEMA', 'MACD']
+#     df_talib = df_stock['close']
+#     for indicator in indicators:
+#         df = None
+#         # Using python's eval function to create a method from a string instead of having every method defined
+#         try:
+#             if indicator == 'MACD':
+#                 df = eval('talib.' + indicator + '(df_talib)')
+#             else:
+#                 df = eval('talib.' + indicator + '(df_talib, 2)')
+#         except:
+#             print(indicator)
+#             continue
+#         # Some method return series, so we can check to convert here
+#         if isinstance(df, pd.Series):
+#             df = df.to_frame()
+#         else:
+#             for s in df:
+#                 df_t = s.to_frame()
+#                 df_t = df_t.add_prefix(indicator + '_')
+#                 # Join merge dataframes based on the date
+#                 df_stock = df_stock.merge(df_t, left_index=True, right_index=True)
+#             continue
+#         # Appropriate labels on each column
+#         df = df.add_prefix(indicator + '_')
+#         # Join merge dataframes based on the date
+#         df_stock = df_stock.merge(df, left_index=True, right_index=True)
+#     # Fix labels
+#     df_stock.columns = df_stock.columns.str.replace(' ', '_')
+#     df_stock.index.name = 'date'
+#     return df_stock
+#
+#
+# def add_indicators_ta(df_stock):
+#     df = tta.add_all_ta_features(df_stock, *COLUMNS)
+#     df_stock = df_stock.merge(df, left_index=True, right_index=True)
+#     # Fix labels
+#     df_stock.columns = df_stock.columns.str.replace(' ', '_')
+#     df_stock.index.name = 'date'
+#     return df_stock
+
+
+def add_indicators_finta(df_stock):
     # Here are all indicators we are using
-    #indicators = ['SMA', 'SMM', 'SSMA', 'EMA', 'DEMA', 'TEMA', 'TRIMA', 'TRIX', 'VAMA', 'ER', 'KAMA', 'ZLEMA', 'WMA', 'HMA', 'EVWMA', 'VWAP', 'SMMA', 'MACD', 'PPO', 'VW_MACD', 'EV_MACD', 'MOM', 'ROC', 'RSI', 'IFT_RSI']
-    indicators = [name for name in list(TA.__dict__.keys()) if str(name).isupper()]
+    indicators = ['SMM', 'SSMA', 'EMA', 'DEMA', 'TEMA', 'TRIX', 'VAMA', 'ER', 'ZLEMA', 'WMA',
+                  'HMA', 'EVWMA', 'VWAP', 'SMMA', 'MACD', 'PPO', 'VW_MACD', 'EV_MACD', 'MOM', 'ROC', 'RSI', 'IFT_RSI']
+    # indicators = [name for name in list(fta.__dict__.keys()) if str(name).isupper()]
     # These indicators need more tuning or are broken
     broken_indicators = ['SAR', 'TMF', 'VR', 'QSTICK']
-    columns = ['open', 'high', 'low', 'close', 'volume']
-    df_fa = df_stock[columns]
+
+    df_fa = df_stock[COLUMNS]
     for indicator in indicators:
         if indicator not in broken_indicators:
             df = None
             # Using python's eval function to create a method from a string instead of having every method defined
             try:
-                df = eval('TA.' + indicator + '(df_fa)')
+                df = eval('finta.' + indicator + '(df_fa)')
             except:
                 print(indicator)
                 continue
@@ -73,6 +154,17 @@ def trim_df_columns(df_stocks_full):
     return df_stocks_full
 
 
+def print_permutation_importance(models, X_val, y_val, df, X):
+    for model, name in models:
+        print(f'Permutation importance to Close for {model[name].__class__.__name__ if name != "clf" else model.__class__.__name__}')
+        r = permutation_importance(model, X_val, y_val, n_repeats=32, random_state=0)
+        for i in r.importances_mean.argsort()[::-1]:
+            #if r.importances_mean[i] - 2 * r.importances_std[i] > 0:
+            print(f"{X.columns[i]:<{df.shape[1]}}"
+                  f"{r.importances_mean[i]:.3f}"
+                  f" +/- {r.importances_std[i]:.3f}")
+        print(' ')
+
 # Create an ensamble method to select best fit model
 def reg_model_metrics(reg_models, X_train, X_test, y_train, y_test):
     """ Function takes in different dictionary of models and training and testing sets,
@@ -101,40 +193,40 @@ def reg_model_metrics(reg_models, X_train, X_test, y_train, y_test):
         y_pred = pred_model.predict(X_test)
         str_models.append(str(model_name))
 
-        #1 & 2 Coefficient of Determination (R² & Adjusted R²)
+        # 1 & 2 Coefficient of Determination (R² & Adjusted R²)
         r2 = r2_score(y_test, y_pred)
         adj_r2 = 1 - (1 - r2) * (len(y_train) - 1) / (len(y_train) - X_train.shape[1] - 1)
         R2_result.append(round(r2, 2))
         adj_R2_result.append(round(adj_r2, 2))
 
-        #3 & 4. MSE and RMSE
+        # 3 & 4. MSE and RMSE
         mse = mean_squared_error(y_pred=y_pred, y_true=y_test, squared=True)
         rmse = mean_squared_error(y_pred=y_pred, y_true=y_test, squared=False)
         MSE_result.append(round(mse, 2))
         RMSE_result.append(round(rmse, 2))
 
-        #5. MAE
+        # 5. MAE
         mae = mean_absolute_error(y_pred=y_pred, y_true=y_test)
         MAE_result.append(round(mae, 2))
 
-        #6. Model training and test scores or accuracies
+        # 6. Model training and test scores or accuracies
         train_score = round(pred_model.score(X_train, y_train) * 100, 2)
         test_score = round(pred_model.score(X_test, y_test) * 100, 2)
 
         training_score.append(train_score)
         testing_score.append(test_score)
 
-        number_of_observations = 50
+        number_of_observations = 60
         index_point = X_test.shape[0] - number_of_observations
         x_ax = X_test.index.tolist()[index_point:]
 
-        vwap = X_test['vwap']
+        #    vwap = X_test['vwap']
 
         plt.figure(figsize=(25, 10))
 
         plt.plot(x_ax, y_test[index_point:], label="Actual", color='red', linewidth=2)
         plt.plot(x_ax, y_pred[index_point:], label="Predicted", color='yellow', linewidth=2)
-        plt.plot(x_ax, vwap[index_point:], label="VWAP", color='blue', linewidth=2)
+        #   plt.plot(x_ax, vwap[index_point:], label="VWAP", color='blue', linewidth=2)
 
         plt.title("JPM Close Prices: Predicted data - Actual using " + model_name)
         plt.xlabel('Years')
@@ -157,20 +249,20 @@ def get_complexity(X_train, X_test, y_train, y_test, complex_num):
     test_mses = []
     r_squared = []
 
-    for i in range(1, complex_num+1):
+    for i in range(1, complex_num + 1):
         poly_ordinal_ohe = make_column_transformer(
             (PolynomialFeatures(include_bias=False, degree=i), make_column_selector(dtype_include=np.number)))
         pipe = Pipeline([('transformer', poly_ordinal_ohe), ('linreg', LinearRegression())])
 
-        #fit on train
+        # fit on train
         pipe.fit(X_train, y_train)
         R_squared = r2_score(y_test, pipe.predict(X_test))
 
-        #predict on train and test
+        # predict on train and test
         p1 = pipe.predict(X_train)
         p2 = pipe.predict(X_test)
 
-        #create MSEs for train and test sets
+        # create MSEs for train and test sets
         train_mses.append(round(mean_squared_error(y_train, p1), 4))
         test_mses.append(round(mean_squared_error(y_test, p2), 4))
         r_squared.append(round(R_squared, 4))
@@ -182,5 +274,6 @@ def get_complexity(X_train, X_test, y_train, y_test, complex_num):
     print(f'The best degree of the polynomial model is:  {best_complexity} out of {complex_num}')
     print(f'The smallest mean squared error on the test dataset is : {best_mse: .4f} out of {test_mses}')
     print(f'The best value of the R-sq of the model, as a good fit is: {best_rsq2: .4f} out of {r_squared}')
-
     return best_complexity
+
+#%%
